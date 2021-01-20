d2a93b50c8fd7aa97769ab0efeaa41b6e45b0af3
Michael Maton <mjmaton6@gmail.com>
2021-01-20 15:05:31 -0600

    POST operation complete

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,46 +1,44 @@
 // scheme-model
-const db = require("../../data/db-config.js")
+const db = require("../../data/db-config.js");
 
 module.exports = {
-    find,
-    findById,
-    findSteps,
-    add,
-    addStep,
-    update,
-    remove,
-}
+  find,
+  findById,
+  findSteps,
+  add,
+  addStep,
+  update,
+  remove,
+};
 
 function find() {
-    return db("schemes")
+  return db("schemes");
 }
 function findById(id) {
-    const schemeObject = db("schemes").where("schemes.id", id);
-    // return (schemeObject ? schemeObject : "id does not exist")
-    if (!schemeObject) {
-        return Promise.resolve(null)
-    } else {
-        return schemeObject;
-    }
+  const schemeObject = db("schemes").where("schemes.id", id);
+  // return (schemeObject ? schemeObject : "id does not exist")
+  if (!schemeObject) {
+    return Promise.resolve(null);
+  } else {
+    return schemeObject;
+  }
 }
 
 function findSteps(id) {
-    return db("steps")
-        .join("schemes", "steps.scheme_id", "schemes.id")
-        .where("steps.scheme_id", id)
-        .select("scheme_name", "instructions", "step_number")
-        .orderBy("step_number")
+  return db("steps")
+    .join("schemes", "steps.scheme_id", "schemes.id")
+    .where("steps.scheme_id", id)
+    .select("scheme_name", "instructions", "step_number")
+    .orderBy("step_number");
 }
 
-function add() {
-
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .then(([id]) => {
+      return findById(id);
+    });
 }
-function addStep() {
-
-}
-function update() {
-
-}
-function remove() {
-
-}
+function addStep() {}
+function update() {}
+function remove() {}
